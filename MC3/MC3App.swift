5b0d7f02dd5f3e4f4444984576eb2631da1218e4
vanessachristie03 <vanessachristie18@gmail.com>
2024-07-11 18:54:48 +0700

    Initial Commit

---
+++ MC3/MC3App.swift
@@ -0,0 +1,17 @@
+//
+//  MC3App.swift
+//  MC3
+//
+//  Created by Vanessa on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
